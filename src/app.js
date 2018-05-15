541b7a9ec27d476c31176035cede59497fd42385
Chris Ewald <itsbewald@gmail.com>
2018-05-15 00:49:21 -0700

    Removed unnecessary testing data from main app.js file to prepare for persisting data once database is added

--- src/app.js
+++ src/app.js
@@ -11,17 +11,6 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.subscribe(() => {
-    const state = store.getState();
-    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-    console.log(visibleExpenses);
-});
-
-const expenseOne = store.dispatch(addExpense({ description: 'Water Bill', amount: 100, createdAt: 90 }));
-const expenseTwo = store.dispatch(addExpense({ description: 'Gas Bill', amount: 50, createdAt: 100 }));
-const expenseThree = store.dispatch(addExpense({ description: 'TV Bill', amount: 200, createdAt: 2000 }));
-const expenseFour = store.dispatch(addExpense({ description: 'Rent', amount: 5000, createdAt: 5000 }));
-
 const jsx = (
 	<Provider store={store}>
 		<AppRouter />
